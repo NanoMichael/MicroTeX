c5c371f1f9902d8594a65d281b52dbee3d7b3ac6
Nano <artiano@hotmail.com>
2018-11-11 21:57:24 +0800

    do not build SAMPLES if build target is Android

--- src/config.h
+++ src/config.h
@@ -1,16 +1,18 @@
 #ifndef CONFIG_H_INCLUDED
 #define CONFIG_H_INCLUDED
 
-// flag for debug
+// Flag for debug
 // #define __DEBUG
 
-// flag for graphic debug
+// Flag for graphic debug
 #define __GA_DEBUG
 
-// flag for memcheck
+// Flag for memcheck
 // #define __MEM_CHECK
 
-// flag for compile samples
-#define __USE_SAMPLES
+// Flag for compile samples
+#ifndef __ANDROID__
+    #define __USE_SAMPLES
+#endif // __ANDROID__
 
 #endif // CONFIG_H_INCLUDED
