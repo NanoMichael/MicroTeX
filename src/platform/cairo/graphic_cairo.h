06240a4b76598adfd7185e7da6698d9ad1d4bed7
Nano <artiano@hotmail.com>
2020-02-04 15:30:01 +0800

    compat MAC OS

--- src/platform/cairo/graphic_cairo.h
+++ src/platform/cairo/graphic_cairo.h
@@ -1,6 +1,6 @@
 #include "config.h"
 
-#if defined(__OS_Linux__) && !defined(MEM_CHECK)
+#if defined(__OS_Unix_like_PC__) && !defined(MEM_CHECK)
 
 #ifndef GRAPHIC_CAIRO_H_INCLUDED
 #define GRAPHIC_CAIRO_H_INCLUDED
@@ -127,4 +127,4 @@ public:
 }  // namespace tex
 
 #endif  // GRAPHIC_CAIRO_H_INCLUDED
-#endif  // __OS_Linux__ && !MEM_CHECK
+#endif  // __OS_Unix_like_PC__ && !MEM_CHECK
