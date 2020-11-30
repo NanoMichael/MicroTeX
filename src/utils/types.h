f77a026a641e854900ebf587e73b76d20b89b7bb
Nano <artiano@hotmail.com>
2020-11-30 23:47:17 +0800

    add MathConsts

---
+++ src/utils/types.h
@@ -0,0 +1,13 @@
+#ifndef TYPES_INCLUDED
+#define TYPES_INCLUDED
+
+#include <cinttypes>
+
+namespace tex {
+
+using int16  = std::int16_t;
+using uint16 = std::uint16_t;
+
+}  // namespace tex
+
+#endif
