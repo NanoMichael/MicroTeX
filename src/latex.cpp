3bdd94b9b7263e1a30325d75aafe275a2f00fc45
sp1rit <sp1rit@disroot.org>
2020-12-06 13:48:22 +0100

    initial support of resource detection

--- src/latex.cpp
+++ src/latex.cpp
@@ -13,8 +13,57 @@ string tex::RES_BASE = "res";
 TeXFormula*       LaTeX::_formula = nullptr;
 TeXRenderBuilder* LaTeX::_builder = nullptr;
 
-void LaTeX::init(const string& res_root_path) {
-  RES_BASE = res_root_path;
+string LaTeX::queryResourceLocation(string& custom_path) {
+    queue<string> paths;
+	paths.push(custom_path);
+
+	// checks if XDG_DATA_HOME exists. If it does, it pushes it to potential paths.
+	char* userdata = getenv("XDG_DATA_HOME");
+	if (userdata != NULL && userdata != "") {
+		paths.push(string(userdata));
+	}
+	delete userdata;
+
+	// checks if XDG_DATA_DIRS is set. If it is, it splits XDG_DATA_DIRS at : and pushes each part of it to potential paths.
+	char* xdg = getenv("XDG_DATA_DIRS");
+	if (xdg != NULL && xdg != "") {
+		stringstream xdg_paths(xdg);
+		string xdg_path;
+		while (getline(xdg_paths, xdg_path, ':')) {
+			paths.push(xdg_path);
+		}
+	}
+
+	// checks if HOME env var is unset. if it isn't it pushes ~/.local/share/clatexmath to potential paths.
+	char* home = getenv("HOME");
+	if (home != NULL && home != "") {
+		char* userdata_fallback;
+		asprintf(&userdata_fallback, "%s/.local/share/clatexmath/", home);
+		paths.push(string(userdata_fallback));
+		delete userdata_fallback;
+	}
+	paths.push("/usr/share/clatexmath/");
+	paths.push("/usr/local/share/clatexmath/");
+
+	// goes through the list of potential paths. if it finds a path that contains .clatexmath-res_root, it returns it. Otherwise I'll throw an error.
+	while (paths.size() > 0) {
+		filesystem::path p = paths.front();
+		p.append(".clatexmath-res_root");
+		if (filesystem::exists(p)) {
+			p.remove_filename();
+			return p.u8string();
+		}
+		paths.pop();
+	}
+    throw "resource location was not found";
+}
+
+void LaTeX::init(string res_root_path) {
+  try {
+    RES_BASE = queryResourceLocation(res_root_path);
+  } catch (const std::string& e) {
+    throw e;
+  }
   if (_formula != nullptr) return;
 
   NewCommandMacro::_init_();
