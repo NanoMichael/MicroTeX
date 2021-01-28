9930d13fa76d9acb0dda8ec78063d1343250af71
Nano <artiano@hotmail.com>
2021-01-29 00:06:22 +0800

    move indeOf from common.h to utils.h

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -3,6 +3,7 @@
 
 #include <cinttypes>
 #include <functional>
+#include <memory>
 
 #define no_copy_assign(T) \
   T(const T&) = delete;   \
@@ -17,6 +18,23 @@ using u16 = std::uint16_t;
 using i32 = std::int32_t;
 using u32 = std::uint32_t;
 using c32 = char32_t;
+
+/** Type alias shared_ptr<T> to sptr<T> */
+template<typename T>
+using sptr = std::shared_ptr<T>;
+
+template<typename T, typename... Args>
+inline sptr<T> sptrOf(Args&& ... args) {
+  return std::make_shared<T>(std::forward<Args>(args)...);
+}
+
+/** Find the position of a value in the vector, return -1 if not found */
+template<class T>
+inline int indexOf(const std::vector<T>& v, const T& x) {
+  auto it = find(v.begin(), v.end(), x);
+  int i = std::distance(v.begin(), it);
+  return (i >= v.size() ? -1 : i);
+}
 
 /** 
  * Binary search for index of the given target.
