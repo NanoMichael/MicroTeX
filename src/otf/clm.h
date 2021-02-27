efc2d39e0dd9023474079da4b6630d90fce3d94b
Nano <artiano@hotmail.com>
2021-02-27 22:19:58 +0800

    reformat code

--- src/otf/clm.h
+++ src/otf/clm.h
@@ -12,29 +12,29 @@ class BinaryFileReader;
 /** Read a Otf from `.clm` file. */
 class CLMReader final {
 private:
-  static void readMeta(Otf& font, BinaryFileReader& reader) ;
+  static void readMeta(Otf& font, BinaryFileReader& reader);
 
-  static void readClassKernings(Otf& font, BinaryFileReader& reader) ;
+  static void readClassKernings(Otf& font, BinaryFileReader& reader);
 
-  static ClassKerning* readClassKerning(BinaryFileReader& reader) ;
+  static ClassKerning* readClassKerning(BinaryFileReader& reader);
 
   static std::pair<u16, u16*> readClassKerningGlyphs(BinaryFileReader& reader);
 
   LigaTable* readLigatures(BinaryFileReader& reader) const;
 
-  static MathConsts* readMathConsts(BinaryFileReader& reader) ;
+  static MathConsts* readMathConsts(BinaryFileReader& reader);
 
-  static KernRecord* readKerns(BinaryFileReader& reader) ;
+  static KernRecord* readKerns(BinaryFileReader& reader);
 
-  static Variants* readVariants(BinaryFileReader& reader) ;
+  static Variants* readVariants(BinaryFileReader& reader);
 
-  static GlyphAssembly* readGlyphAssembly(BinaryFileReader& reader) ;
+  static GlyphAssembly* readGlyphAssembly(BinaryFileReader& reader);
 
-  static Math* readMath(BinaryFileReader& reader) ;
+  static Math* readMath(BinaryFileReader& reader);
 
-  static Glyph* readGlyph(bool isMathFont, BinaryFileReader& reader) ;
+  static Glyph* readGlyph(bool isMathFont, BinaryFileReader& reader);
 
-  static void readGlyphs(Otf& font, BinaryFileReader& reader) ;
+  static void readGlyphs(Otf& font, BinaryFileReader& reader);
 
 public:
   no_copy_assign(CLMReader);
