0c41c32e2d5d72df10625a659a6812901841096c
Nano <artiano@hotmail.com>
2021-07-13 02:00:42 +0800

    ScriptsAtom done

--- src/atom/atom_scripts.cpp
+++ src/atom/atom_scripts.cpp
@@ -29,10 +29,10 @@ sptr<Box> ScriptsAtom::createBox(Env& env) {
 
   // params to place scripts
   sptr<Box> kernel, base;
-  float delta = 0;
+  float delta = 0.f;
   bool isSymbol = false;
 
-  auto checkSym = [&](const sptr<Atom>& atom, Env& targetEnv) {
+  auto checkSym = [&delta, &isSymbol](const sptr<Atom>& atom, Env& targetEnv) {
     if (auto cs = dynamic_cast<CharSymbol*>(atom.get()); cs != nullptr) {
       if (!cs->isMarkedAsTextSymbol()) {
         delta = cs->getChar(targetEnv).italic();
@@ -70,23 +70,38 @@ sptr<Box> ScriptsAtom::createBox(Env& env) {
   }
 
   const auto& math = env.mathConsts();
-  // calculate the raw shift
-  const auto shiftUp = [&]() {
+  // functions calculate the raw shift
+  const auto calSupShift = [isSymbol, &math, &kernel, &env](const sptr<Box>& box) {
+    float shift = 0.f;
     if (isSymbol) {
-      return -(
+      shift = -(
         env.isCrampedStyle()
         ? math.superscriptShiftUpCramped() * env.scale()
         : math.superscriptShiftUp() * env.scale()
       );
     } else {
-      return -kernel->_height + math.superscriptBaselineDropMax() * env.scale();
+      shift = -kernel->_height + math.superscriptBaselineDropMax() * env.scale();
     }
-  }();
-  const auto shiftDown = (
-    isSymbol
-    ? math.subscriptShiftDown() * env.scale()
-    : math.subscriptBaselineDropMin() * env.scale() + kernel->_depth
-  );
+    const auto gapMin = math.superscriptBottomMin() * env.scale();
+    return (
+      shift + box->_depth > gapMin
+      ? -box->_depth - gapMin
+      : shift
+    );
+  };
+  const auto calSubShift = [isSymbol, &math, &kernel, &env](const sptr<Box>& box) {
+    const auto shift = (
+      isSymbol
+      ? math.subscriptShiftDown() * env.scale()
+      : math.subscriptBaselineDropMin() * env.scale() + kernel->_depth
+    );
+    const auto gapMax = math.subscriptTopMax() * env.scale();
+    return (
+      box->_height - shift > gapMax
+      ? box->_height - gapMax
+      : shift
+    );
+  };
 
   auto scriptSpace = SpaceAtom(UnitType::mu, 0.5f, 0.f, 0.f).createBox(env);
   auto createBox = [&](const sptr<Atom>& atom, TexStyle style) {
@@ -100,29 +115,42 @@ sptr<Box> ScriptsAtom::createBox(Env& env) {
     // 1. both super and sub scripts
     auto sup = env.withStyle(env.supStyle(), [&](Env& sup) { return _sup->createBox(sup); });
     auto sub = env.withStyle(env.subStyle(), [&](Env& sub) { return _sub->createBox(sub); });
+    // calculate shift
+    const auto shiftUp = calSupShift(sup);
+    const auto shiftDown = calSubShift(sub);
     // calculate gap between super & sub script
-    const auto gapMin = math.subSuperscriptGapMin() * env.scale();
     const auto supBottomPos = shiftUp + sup->_depth;
     const auto subTopPos = shiftDown - sub->_height;
-    auto gap = std::max(subTopPos - supBottomPos, gapMin);
+    const auto gap = subTopPos - supBottomPos;
     // add super & sub script to vertical box
     auto v = sptrOf<VBox>();
-    sup->_shift = kernel->_width - base->_width + delta;
+    sup->_shift = delta;
     v->add(sup);
-    v->add(sptrOf<StrutBox>(0.f, gap, 0.f, 0.f));
+    const auto gapMin = math.subSuperscriptGapMin() * env.scale();
+    v->add(sptrOf<StrutBox>(0.f, std::max(gap, gapMin), 0.f, 0.f));
     v->add(sub);
-    //
+    // shift vertical box
+    const auto height = v->_height + v->_depth;
+    if (gap > gapMin) {
+      v->_depth = sub->_depth;
+      v->_height = height - sub->_depth;
+      v->_shift = shiftDown;
+    } else {
+      v->_height = sup->_height;
+      v->_depth = height - sup->_height;
+      v->_shift = -math.superscriptBottomMaxWithSubscript() * env.scale() - sup->_depth;
+    }
+    // add to horizontal box
+    auto h = sptrOf<HBox>(base);
+    const auto kern = kernel->_width - base->_width;
+    if (std::abs(kern) > PREC) h->add(StrutBox::create(kern));
+    h->add(v);
+    h->add(scriptSpace);
+    return h;
   } else if (_sup == nullptr) {
     // 2. only subscript
     auto sub = createBox(_sub, env.subStyle());
-    // calculate shift
-    const auto gapMax = math.subscriptTopMax() * env.scale();
-    sub->_shift = (
-      sub->_height - shiftDown > gapMax
-      ? sub->_height - gapMax
-      : shiftDown
-    );
-    // add to horizontal box
+    sub->_shift = calSubShift(sub);
     auto h = sptrOf<HBox>(base);
     // add kern if kernel box and base box are different
     const auto kern = kernel->_width - base->_width;
@@ -132,13 +160,7 @@ sptr<Box> ScriptsAtom::createBox(Env& env) {
   } else if (_sub == nullptr) {
     // 3. only superscript
     auto sup = createBox(_sup, env.supStyle());
-    // calculate shift
-    const auto gapMin = math.superscriptBottomMin() * env.scale();
-    sup->_shift = (
-      shiftUp + sup->_depth > gapMin
-      ? -sup->_depth - gapMin
-      : shiftUp
-    );
+    sup->_shift = calSupShift(sup);
     // add to horizontal box
     auto h = sptrOf<HBox>(base);
     const auto kern = kernel->_width - base->_width + delta;
