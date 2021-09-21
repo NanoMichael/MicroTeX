2ed7f7161b89036fdd16f1e155a498ccd161e250
Nano <artiano@hotmail.com>
2021-09-21 23:43:28 +0800

    cmake build script add installs

---
+++ prebuilt/otf2clm.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+usage="
+Parse clm data from OTF font file.
+
+Usage: $(basename $0)
+
+Required arguments:
+    1: OTF font file path
+    2: true or false to tell the program whether the font is a math font
+    3: target file to save the clm data
+"
+
+if [ $# -ne 3 ]
+then
+  echo "$usage"
+  exit 1
+fi
+
+fontforge -lang=py -script otf2clm.py $1 $2 $3
+
+echo "Generated clm data was saved into file: $3"
