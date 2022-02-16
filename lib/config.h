9e00cbc6410914bd837d08a68b4000dc41cd4eb4
Nano <artiano@hotmail.com>
2022-02-17 00:35:17 +0800

    compile: only export interfaces

--- lib/config.h
+++ lib/config.h
@@ -24,14 +24,19 @@
 #endif
 
 // export
+// msvc
 #ifdef _MSC_VER
-#if defined(TINYTEX_LIBRARY)
-#define TINYTEX_EXPORT __declspec(dllexport)
+#   if defined(TINYTEX_LIBRARY)
+#       define TINYTEX_EXPORT __declspec(dllexport)
+#   else
+#       define TINYTEX_EXPORT __declspec(dllimport)
+#   endif
+// gnuc and clang
+#elif defined(__GNUC__) || defined(__GNUG__) || defined(__clang__)
+#   define TINYTEX_EXPORT __attribute((visibility("default")))
+// otherwise...
 #else
-#define TINYTEX_EXPORT __declspec(dllimport)
-#endif
-#else
-#define TINYTEX_EXPORT
+#   define TINYTEX_EXPORT
 #endif
 
 #endif  // TINYTEX_CONFIG_H
