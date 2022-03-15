575aa54acfdf51df2fba29bd7f13603ce2a46ed4
Nano <artiano@hotmail.com>
2022-03-16 02:03:26 +0800

    fix: command 'not' positioned incorrectly

--- lib/atom/atom_accent.cpp
+++ lib/atom/atom_accent.cpp
@@ -78,21 +78,37 @@ sptr<Box> AccentedAtom::createBox(Env& env) {
 
   // add to vertical box
   auto vbox = new VBox();
-  vbox->add(accenter);
   // kerning
-  const auto delta = (
-    _fakeAccent
-    ? Units::fsize(UnitType::mu, 1, env)
-    : -min(accentee->_height, env.xHeight())
-  );
+  auto delta = 0.f;
+  auto sigma = 0.f;
+  if (_fakeAccent) {
+    delta = Units::fsize(UnitType::mu, 1, env);
+  } else if (accenter->_depth <= 0) {
+    delta = -min(accentee->_height, (float) env.mathConsts().accentBaseHeight());
+  } else {
+    // if accent has depth (e.g. \not), we need to align the accentee and accenter
+    // by its baseline
+    if (accentee->_height > accenter->_height) {
+      auto t = accenter;
+      accenter = accentee;
+      accentee = t;
+    }
+    delta = -(accentee->_height + accenter->_depth);
+    sigma = max(accenter->_depth - accentee->_depth, 0.f);
+  }
+
+  vbox->add(accenter);
   vbox->add(sptrOf<StrutBox>(0.f, delta, 0.f, 0.f));
-  // accentee
   vbox->add(accentee);
 
   // set height and depth of the vertical box
-  const auto total = vbox->_height + vbox->_depth;
+  vbox->_height = vbox->vlen() - accentee->_depth;
   vbox->_depth = accentee->_depth;
-  vbox->_height = total - accentee->_depth;
+
+  // add extra space to make sure the box is tall enough to wrap all children
+  if (sigma > PREC) {
+    vbox->add(sptrOf<StrutBox>(0.f, sigma, 0.f, 0.f));
+  }
 
   return sptr<Box>(vbox);
 }
