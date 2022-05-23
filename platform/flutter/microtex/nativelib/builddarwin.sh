2c08f6d7886cee15a67490d488639f4082762df1
Nano <artiano@hotmail.com>
2022-05-23 21:23:55 +0800

    support for apple sillicon

--- platform/flutter/microtex/nativelib/builddarwin.sh
+++ platform/flutter/microtex/nativelib/builddarwin.sh
@@ -4,14 +4,21 @@ RED="\033[0;31m"
 GREEN="\033[0;32m"
 RST="\033[0m"
 
+supported_ios_archs=("OS64" "SIMULATOR64" "SIMULATORARM64")
+supported_mac_archs=("MAC" "MAC_ARM64")
+
 function usage {
 cat << EOF
 Build microtex static library for iOS and macOS.
 
 Options:
     -t | --target
-      the target to build, currently support iOS (arm64 only, for real device), simulator (x86_64
-      only, for iOS simulator), macOS (x86_64 only)
+      the target to build, currently support following values:
+      - OS64           | arm64   | build for iOS device
+      - SIMULATOR64    | x86_64  | build for iOS simulator
+      - SIMULATORARM64 | arm64   | build for iOS simulator
+      - MAC            | x86_64  | build for macOS
+      - MAC_ARM64      | arm64   | build for macOS on Apple Sillicon
 
     -b | --buildtype
       the build type, must be one of the Debug and Release, use Debug if not specified
@@ -20,6 +27,14 @@ Options:
       print this message and exit
 EOF
 exit 0
+}
+
+function info {
+  echo -e "${GREEN}$1${RST}"
+}
+
+function err {
+  echo -e "${RED}$1${RST}"
 }
 
 target=""
@@ -44,26 +59,21 @@ while [[ $# -gt 0 ]]; do
 done
 
 if [ ! $target ]; then
-  echo -e "${RED}No target specified, exit${RST}"
+  err "No target specified, exit"
   usage
 fi
 
 if [ ! $build_type = "Debug" -a ! $build_type = 'Release' ]; then
-  echo -e "${RED}Not supported build type: $build_type$RST"
+  err "Not supported build type: $build_type"
   usage
 fi
 
-if [ "$target" = "iOS" ]; then
-  echo -e "${GREEN}Build for iOS (arm64, $build_type)...${RST}"
-  platform="OS64"
-elif [ "$target" = "simulator" ]; then
-  echo -e "${GREEN}Build for iOS simulator (x86_64, $build_type)...${RST}"
-  platform="SIMULATOR64"
-elif [ "$target" = "macOS" ]; then
-  echo -e "${GREEN}Build for macOS (x86_64, $build_type)...${RST}"
-  platform="MAC"
+if [[ "${supported_ios_archs[*]}" =~ $target ]]; then
+  info "Build for iOS ($target, $build_type)..."
+elif [[ "${supported_mac_archs[*]}" =~ $target ]]; then
+  info "Build for macOs ($target, $build_type)..."
 else
-  echo -e "${RED}Not supported target: ${target}${RST}"
+  err "Not supported target, ${target}, exit"
   exit 1
 fi
 build_dir="build_${target}_${build_type}"
@@ -81,16 +91,16 @@ fi
 cd "$build_dir" || exit 1
 cmake  .. -G Xcode \
   -DCMAKE_TOOLCHAIN_FILE=../ios.toolchain.cmake \
-  -DPLATFORM=$platform \
+  -DPLATFORM=$target \
   -DIS_OSX_IOS=1
 cmake --build . --config $build_type --target install
 
 dst=""
-if [ $target = "iOS" -o $target = "simulator" ]; then
+if [[ "${supported_ios_archs[*]}" =~ $target ]]; then
   dst="../../ios/libs/"
 else
   dst="../../macos/libs/"
 fi
 
-echo -e "${GREEN}Copy libs into: $dst$RST"
+info "Copy libs into: $dst"
 cp -r products/lib/ $dst
