1830294771ea5850a2577d534f6e0c7bc4c21767
Nano <artiano@hotmail.com>
2022-05-30 02:26:01 +0800

    example: use regex match

--- platform/flutter/microtex/example/lib/widget/rich.dart
+++ platform/flutter/microtex/example/lib/widget/rich.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:microtex/microtex.dart';
+import 'package:microtex_example/utils/convert_spans.dart';
 
 class SimpleRichText extends StatelessWidget {
   final String text;
@@ -12,61 +12,12 @@ class SimpleRichText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return RichText(
-      text: TextSpan(children: _buildSpans()),
+      text: TextSpan(
+        children: SpanConverter().buildSpans(
+          text: text,
+          style: const TextStyle(color: Colors.black, fontSize: 20),
+        ),
+      ),
     );
-  }
-
-  static const r1 = r'\$\$[\s\S]*?\$\$';
-  static const r2 = r'\$[\s\S]*?\$';
-
-  _buildSpans() {
-    final r = RegExp([r1, r2].join('|'));
-    final children = <InlineSpan>[];
-    text.splitMapJoin(
-      r,
-      onNonMatch: (span) {
-        if (span.isEmpty) return span;
-        children.add(
-          TextSpan(
-            text: span,
-            style: const TextStyle(color: Colors.black, fontSize: 20),
-          ),
-        );
-        return span;
-      },
-      onMatch: (m) {
-        final tex = m[0] ?? '';
-        if (tex.isEmpty) return '';
-        final isInline = !tex.startsWith('\$\$');
-        children.add(WidgetSpan(
-          alignment: PlaceholderAlignment.baseline,
-          baseline: TextBaseline.alphabetic,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: _buildLaTeX(tex, isInline),
-          ),
-        ));
-        return tex.toString();
-      },
-    );
-    return children;
-  }
-
-  _buildLaTeX(String tex, bool isInline) {
-    final t = LaTeX(
-      latex: tex,
-      textSize: 24,
-      color: Colors.black,
-      blur: const Blur(BlurStyle.normal, 0.34),
-    );
-    if (isInline) {
-      return t;
-    }
-    return LayoutBuilder(builder: (context, constraints) {
-      return SizedBox(
-        width: constraints.maxWidth,
-        child: Center(child: t),
-      );
-    });
   }
 }
--- platform/flutter/microtex/example/lib/widget/tex_highlight_controller.dart
+++ platform/flutter/microtex/example/lib/widget/tex_highlight_controller.dart
@@ -3,43 +3,64 @@ import 'package:flutter/material.dart';
 class TeXHighlightController extends TextEditingController {
   TeXHighlightController({String? text}) : super(text: text);
 
+  static final out = RegExp(r'\${1,2}[\s\S]*?\${1,2}');
+  static final cmd = RegExp(r'\${1,2}|\\[a-zA-Z]*|\_|\^|\{|\}');
+
   @override
   TextSpan buildTextSpan({
     required BuildContext context,
     TextStyle? style,
     required bool withComposing,
   }) {
-    final dollarStyle = style?.copyWith(color: Colors.amberAccent) ??
-        const TextStyle(color: Colors.amberAccent);
-    final texStyle = style?.copyWith(color: Colors.green) ??
-        const TextStyle(color: Colors.green);
-    final cmdStyle = style?.copyWith(color: Colors.blue) ??
-        const TextStyle(color: Colors.blue);
+    final sty = style ?? const TextStyle();
+    final dollarSty = sty.copyWith(color: Colors.deepOrange);
+    final texStyle = sty.copyWith(color: Colors.green);
+    final cmdStyle = sty.copyWith(color: Colors.blue);
+    final keyStyle = sty.copyWith(color: Colors.purple);
 
     final children = <TextSpan>[];
-    var k = 0;
-    var isInTeX = false;
-    for (var i = 0; i < text.length; i++) {
-      final c = text[i];
-      if (c == '\$') {
-        if (!isInTeX && k != i) {
-          final str = text.substring(k, i);
-          children.add(TextSpan(text: str, style: style));
-        }
-        var j = i + 1;
-        // consume the '$'
-        while (j < text.length && text[j] == '\$') {
-          j++;
-        }
-        i = j;
-        final str = text.substring(i, j);
-        if (str.length <= 2) {
-          isInTeX = !isInTeX;
-        }
-        children.add(TextSpan(text: str, style: dollarStyle));
-        k = i;
-      }
+
+    matchTeX(String tex) {
+      tex.splitMapJoin(
+        cmd,
+        onNonMatch: (span) {
+          if (span.isEmpty) return span;
+          children.add(TextSpan(text: span, style: texStyle));
+          return span;
+        },
+        onMatch: (m) {
+          final cmd = m[0] ?? '';
+          if (cmd.isEmpty) return '';
+          final TextStyle s;
+          if (cmd == '\$' || cmd == '\$\$') {
+            s = dollarSty;
+          } else if (cmd == '_' || cmd == '^') {
+            s = keyStyle;
+          } else if (cmd == '{' || cmd == '}') {
+            s = sty;
+          } else {
+            s = cmdStyle;
+          }
+          children.add(TextSpan(text: cmd, style: s));
+          return cmd;
+        },
+      );
     }
+
+    text.splitMapJoin(
+      out,
+      onNonMatch: (span) {
+        if (span.isEmpty) return span;
+        children.add(TextSpan(text: span, style: style));
+        return span;
+      },
+      onMatch: (m) {
+        final tex = m[0] ?? '';
+        if (tex.isEmpty) return '';
+        matchTeX(tex);
+        return tex;
+      },
+    );
     return TextSpan(style: style, children: children);
   }
 }
